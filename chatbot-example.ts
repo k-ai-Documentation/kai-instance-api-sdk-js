6d107fa1b02d234bf18f42923ac96a929c1c152e
xubian0619 <xubian01125113@icloud.com>
2025-01-24 16:24:12 +0800

    update chatbot example

--- chatbot-example.ts
+++ chatbot-example.ts
@@ -12,7 +12,7 @@ export class Credentials implements KaiStudioCredentials {
     }
 }
 
-let credentials = new Credentials('c977644b-8b4a-43ee-8901-a609ef3b9e19', '03fb090f-b2ac-444a-8356-6be9d9fe132a', 'TTLM0qFAT9FTXk/i6uvwh8IbU3jLw9/zMXvdSul2/E0=');
+let credentials = new Credentials('xxxx', 'xxxxx', 'xxxxxx');
 
 let kaiStudio = new KaiStudio(credentials);
 let chatbotInstance = kaiStudio.chatbot()
