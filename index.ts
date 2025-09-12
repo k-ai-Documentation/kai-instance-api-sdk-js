a249733c0af27b37f58bdbf66acb57d7e963fda6
RunzhongK-AI <rmei@k-ai.ai>
2025-09-12 19:25:52 +0200

    udpqte

--- index.ts
+++ index.ts
@@ -52,11 +52,11 @@ export class KaiStudioInstance {
 
         if (this.credentials.host) {
             baseUrl = this.credentials.host
-            if (this.credentials.instanceId && this.credentials.apiKey) {
-                headers = {
-                    'api-key': this.credentials.apiKey,
-                    'instance-id': this.credentials.instanceId
-                }
+            if (this.credentials.apiKey) {
+                headers['api-key'] = this.credentials.apiKey
+            }
+            if (this.credentials.instanceId ) {
+                headers['instance-id'] = this.credentials.instanceId
             }
         }
 
